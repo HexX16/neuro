0c956ad2ba4d7c4626cf8e1f5546d7a0c0e2662f
Viktor <viktorzu6@gmail.com>
2026-03-05 22:01:30 +0300

    05_03_26

--- study_ml/pure_python_ml/models/lin_reg.py
+++ study_ml/pure_python_ml/models/lin_reg.py
@@ -1,45 +1,31 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from models_utils import LinearRegression as Lr
 
 np.random.seed(42)
 
-x = np.random.rand(100)
-true_w = 4
-true_b = 3
-y = x * true_w + true_b + np.random.randn(100)
-
-w = np.random.rand()
-b = np.random.rand()
-
-def predict(x, w, b):
-    y_pred = x*w + b
-    return y_pred
-
-def loss(y, y_pred):
-    return np.mean((y_pred - y)**2)
-
-def grad(x, y, y_pred):
-    dw = np.mean(2*x*(y_pred-y))
-    db = np.mean(2*(y_pred-y))
-    return dw, db
-
-def train(x, y, w, b, epochs=1000, lr = 0.01):
-    for i in range(epochs):
-        y_pred = predict(x, w, b)
-        dw, db = grad(x, y, y_pred)
-        w -= dw*lr
-        b -= db*lr
-
-        if i%100==0:
-            print(f"{i}: loss = {loss(y, y_pred)}, w = {w}, b = {b}")
-
-    return w,b
-
-train(x, y, w, b)
-print(true_w,true_b)
-        
+n = 30
+power = np.random.randint(100,300,n)
+engine_volume = np.round(np.random.uniform(2.0, 4.0, n), 1)
+max_torque = np.random.randint(200, 400, n)
+x = np.column_stack([power, engine_volume, max_torque])
+w = [200, 5000, 100]
+b = 10000
+noise = np.random.normal(0, 2000, n)
+price = x.dot(w) + b + noise
 
 
-# plt.scatter(x, y)   
-# plt.show()
+df = pd.DataFrame({
+    'power': power,
+    'engine_volume': engine_volume,
+    'max_turque': max_torque,
+    'price': price
+    })
+
+model = Lr()
+model.fit(x, price)
+
+pred = model.predict(x)
+print(pred[:5])
+print(price[:5])
---
+++ study_ml/pure_python_ml/models/lin_reg_one_fea.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from models_utils import LinearRegressionOneFeature as LrOneFea
+np.random.seed(42)
+
+true_w = 4
+true_b = 3
+x = np.random.rand(100)
+y = x * true_w + true_b + np.random.randn(100)
+
+model = LrOneFea()
+model.fit(x, y)
+
+y_pred = model.predict(x)
+
+plt.scatter(x, y, label = 'Истинные метки', color = 'blue')   
+plt.plot(x, y_pred, label = 'Предсказания', color ='red')
+plt.show()
